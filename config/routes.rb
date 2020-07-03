8ee60336a503a4342ac965f92b2d1035b4fb30f7
kaiomagalhaes <me@kaiomagalhaes.com>
2020-07-03 08:28:44 -0300

    remove jira controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
 
   resources :users
   resources :repositories
-  resources :jira
 
   root 'application#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
