18055a5c86932878b08b7440848e6ec0114817f3
kaiomagalhaes <me@kaiomagalhaes.com>
2020-07-10 23:03:57 -0300

    create commits on close pull request

--- app/services/flows/close_pull_request_flow.rb
+++ app/services/flows/close_pull_request_flow.rb
@@ -21,6 +21,17 @@ module Flows
       slack_message.save!
 
       Clients::Github::Branch.new.delete(repository.full_name, pull_request.head)
+
+      commits = Clients::Github::PullRequest.new.list_commits(repository.full_name, pull_request.github_id)
+      commits.each do |commit|
+        Commit.create!(
+          pull_request: pull_request,
+          sha: commit[:sha],
+          author_name: commit[:commit][:author][:name],
+          author_email: commit[:commit][:author][:email],
+          message: commit[:commit][:message]
+        )
+      end
     end
 
     # @TODO: check if pull request is already closed
