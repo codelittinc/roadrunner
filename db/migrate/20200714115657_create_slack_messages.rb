dbc85ed1d35c37c82be4e9236305c747553b2b2a
kaiomagalhaes <me@kaiomagalhaes.com>
2020-07-14 09:02:30 -0300

    create slack message model

---
+++ db/migrate/20200714115657_create_slack_messages.rb
@@ -0,0 +1,11 @@
+class CreateSlackMessages < ActiveRecord::Migration[6.0]
+  def change
+    create_table :slack_messages do |t|
+      t.string :ts
+
+      t.belongs_to :pull_request
+
+      t.timestamps
+    end
+  end
+end
