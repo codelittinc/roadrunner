3a3635548a93b23e431efeaa733a9408f34dd929
kaiomagalhaes <me@kaiomagalhaes.com>
2020-07-20 09:14:20 -0300

    create create and delete github methods for releases

--- app/services/clients/github/release.rb
+++ app/services/clients/github/release.rb
@@ -4,6 +4,19 @@ module Clients
       def list(repository)
         @client.list_releases(repository)
       end
+
+      def create(repository, tag_name, target, body, prerelease)
+        @client.create_release(repository, tag_name, {
+                                 target_commitish: target,
+                                 body: body,
+                                 prerelease: prerelease
+                               })
+      end
+
+      # @TODO: review this method, it does not seem to work
+      def delete(url)
+        @client.delete_release(url)
+      end
     end
   end
 end
--- spec/services/clients/github/release_spec.rb
+++ spec/services/clients/github/release_spec.rb
@@ -3,10 +3,38 @@ require 'external_api_helper'
 
 RSpec.describe Clients::Github::Release, type: :service do
   describe '#list' do
-    it 'returns a list of releases' do
+    xit 'returns a list of releases' do
       VCR.use_cassette('github#release#list') do
         commits = described_class.new.list('codelittinc/codelitt-v2')
         expect(commits.size).to eql(30)
+      end
+    end
+  end
+
+  describe '#create' do
+    xit 'creates a release' do
+      VCR.use_cassette('github#release#create') do
+        repo = 'codelittinc/gh-hooks-repo-test'
+        tag_name = 'rc.3.v0.0.29'
+        body = "this \n is \n nice"
+        described_class.new.create(repo, tag_name, 'master', body, true)
+
+        release = described_class.new.list(repo).first
+
+        expect(release[:tag_name]).to eql(tag_name)
+      end
+    end
+  end
+
+  describe '#delete' do
+    it 'deletes a release' do
+      VCR.use_cassette('github#release#delete') do
+        repo = 'codelittinc/gh-hooks-repo-test'
+        release_to_delete = described_class.new.list(repo).first
+        described_class.new.delete(release_to_delete[:url])
+        release_to_check = described_class.new.list(repo).first
+
+        expect(release_to_delete[:tag_name]).not_to eql(release_to_check[:tag_name])
       end
     end
   end
