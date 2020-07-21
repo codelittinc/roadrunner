c01fdb5b02bd768b612464c44c7feba748dbdfd2
kaiomagalhaes <me@kaiomagalhaes.com>
2020-07-21 09:41:35 -0300

    create branch_compare_message method

--- app/services/messages/builder.rb
+++ app/services/messages/builder.rb
@@ -12,5 +12,22 @@ module Messages
     def self.close_pull_request_message(pull_request)
       "~#{new_pull_request_message(pull_request)}~"
     end
+
+    def self.branch_compare_message(commits)
+      title = "Available in this release *candidate*:\n"
+      prs = commits.map(&:pull_request).uniq(&:id)
+      points = prs.map do |pull_request|
+        base = " - #{pull_request.title}"
+        jira_links = extract_jira_codes(pull_request.description).map do |jira_code|
+          "<https://codelitt.atlassian.net/browse/#{jira_code}|#{jira_code}>"
+        end.join(' ')
+        "#{base} #{jira_links}"
+      end.join("\n")
+      title + points
+    end
+
+    def self.extract_jira_codes(text)
+      text.scan(%r{https?://codelitt.atlassian.net/browse/([A-Z]+-\d+)}).flatten
+    end
   end
 end
---
+++ spec/services/messages/builder_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe Messages::Builder, type: :service do
+  describe '.branch_compare_message' do
+    it 'returns a formatted message ' do
+      pr1 = FactoryBot.create(:pull_request, {
+                                title: 'Add leaseExpirationSized rest of expirations value',
+                                description: 'Card: https://codelitt.atlassian.net/browse/AYAPI-274'
+                              })
+
+      c1 = FactoryBot.create(:commit, {
+                               sha: '123456',
+                               message: 'Fix this thing\n
+          this is my description
+        ',
+                               pull_request: pr1
+                             })
+
+      c2 = FactoryBot.create(:commit, {
+                               sha: '13529',
+                               message: 'Update that feature\n
+          this is my second description
+        ',
+                               pull_request: pr1
+                             })
+
+      pr2 = FactoryBot.create(:pull_request, {
+                                title: 'Filter activity feed based on user preferences',
+                                description: %{
+          If applied, this pull request will make the activity feed filter items based on the user's preferences (market and property type).
+
+          ### Other minor changes:
+          - Fixed unescaped character in a regex constant
+          - Add an "empty state" component to show when no activities are available
+          - Removed components and functions to handle the recent searches, as they will be replaced in the next PR with the homepage activity feed
+
+          ### Card Link:
+          https://codelitt.atlassian.net/browse/HUB-56
+          https://codelitt.atlassian.net/browse/HUB-469
+        }
+                              })
+
+      c3 = FactoryBot.create(:commit, {
+                               sha: '123456',
+                               message: 'Filter activity feed based on user preferences',
+                               pull_request: pr2
+                             })
+
+      message = %(Available in this release *candidate*:
+ - Add leaseExpirationSized rest of expirations value <https://codelitt.atlassian.net/browse/AYAPI-274|AYAPI-274>
+ - Filter activity feed based on user preferences <https://codelitt.atlassian.net/browse/HUB-56|HUB-56> <https://codelitt.atlassian.net/browse/HUB-469|HUB-469>)
+
+      expect(described_class.branch_compare_message([c1, c2, c3])).to eq(message)
+    end
+  end
+end
