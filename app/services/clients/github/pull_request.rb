2c10db7bdfcc1dd0907d650cf667b702ae6f1562
kaiomagalhaes <me@kaiomagalhaes.com>
2020-07-29 01:30:43 -0300

    add get method to github pull request

--- app/services/clients/github/pull_request.rb
+++ app/services/clients/github/pull_request.rb
@@ -1,6 +1,10 @@
 module Clients
   module Github
     class PullRequest < GithubBase
+      def get(repository, github_id)
+        @client.pull_request(repository, github_id)
+      end
+
       def list_commits(repository, github_id)
         @client.pull_request_commits(repository, github_id)
       end
