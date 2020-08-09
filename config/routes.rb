e12d2b7b00a8c3a8a479187a17e7f96ef225ddc3
kaiomagalhaes <me@kaiomagalhaes.com>
2020-08-09 15:58:57 -0300

    create open pull requests controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   resources :users
   resources :repositories
   resources :servers
+  resources :open_pull_requests, only: :index
 
   root 'application#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
