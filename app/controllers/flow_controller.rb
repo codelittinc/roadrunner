036e685cbf9ec74f194df9b163a491059dc99c78
kaiomagalhaes <me@kaiomagalhaes.com>
2020-08-12 12:30:37 -0300

    remove flow execution inside threads

--- app/controllers/flow_controller.rb
+++ app/controllers/flow_controller.rb
@@ -1,11 +1,9 @@
 class FlowController < ApplicationController
   def create
-    Thread.new do
-      FlowExecutor.new((params[:flow] || params).merge(request.GET)).execute
-    end
-
     render json: {
       status: 200
     }, status: :ok
+
+    FlowExecutor.new((params[:flow] || params).merge(request.GET)).execute
   end
 end
