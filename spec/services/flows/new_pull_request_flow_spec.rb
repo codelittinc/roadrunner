c5014500f067413667931919ee66fd9312dc56a3
Rheniery <rhenierymendes@gmail.com>
2020-08-19 16:53:15 -0300

    prevent the creation of pull request when it is already in the database

--- spec/services/flows/new_pull_request_flow_spec.rb
+++ spec/services/flows/new_pull_request_flow_spec.rb
@@ -7,10 +7,38 @@ RSpec.describe Flows::NewPullRequestFlow, type: :service do
   end
 
   describe '#flow?' do
-    context 'when it has the opened action' do
-      it 'returns true ' do
-        flow = described_class.new(valid_json)
+    context 'returns true' do
+      it 'when it has the opened action and does not exist the pull request in database' do
+        valid_json_confirmed = valid_json.deep_dup
+        valid_json_confirmed[:number] = 1
+        valid_json_confirmed[:action] = 'opened'
+        flow = described_class.new(valid_json_confirmed)
         expect(flow.flow?).to be_truthy
+      end
+
+      it 'when it has the ready_for_review action and does not exist the pull request in database' do
+        valid_json_confirmed = valid_json.deep_dup
+        valid_json_confirmed[:number] = 1
+        valid_json_confirmed[:action] = 'ready_for_review'
+        flow = described_class.new(valid_json_confirmed)
+        expect(flow.flow?).to be_truthy
+      end
+    end
+
+    context 'return false when' do
+      it 'when the action is different from opened or ready_for_review' do
+        invalid_json = valid_json.deep_dup
+        invalid_json[:action] = 'dfsdfsdf'
+        flow = described_class.new(invalid_json)
+        expect(flow.flow?).to be_falsey
+      end
+
+      it 'pull request already exists in database' do
+        repository = FactoryBot.create(:repository, name: 'roadrunner-rails')
+        FactoryBot.create(:pull_request, repository: repository, github_id: 160)
+
+        flow = described_class.new(valid_json)
+        expect(flow.flow?).to be_falsey
       end
     end
   end
