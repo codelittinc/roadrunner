746b779641ad5935cf1cc4e596488356d565ce56
kaiomagalhaes <me@kaiomagalhaes.com>
2020-09-04 09:57:07 -0300

    remove ignore from github release tests

--- spec/services/clients/github/release_spec.rb
+++ spec/services/clients/github/release_spec.rb
@@ -3,7 +3,7 @@ require 'external_api_helper'
 
 RSpec.describe Clients::Github::Release, type: :service do
   describe '#list' do
-    xit 'returns a list of releases' do
+    it 'returns a list of releases' do
       VCR.use_cassette('github#release#list') do
         commits = described_class.new.list('codelittinc/codelitt-v2')
         expect(commits.size).to eql(30)
@@ -12,7 +12,7 @@ RSpec.describe Clients::Github::Release, type: :service do
   end
 
   describe '#create' do
-    xit 'creates a release' do
+    it 'creates a release' do
       VCR.use_cassette('github#release#create') do
         repo = 'codelittinc/gh-hooks-repo-test'
         tag_name = 'rc.3.v0.0.29'
