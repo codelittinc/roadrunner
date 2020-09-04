a389ab323883d63e39135b721cadc7fc3d002239
kaiomagalhaes <me@kaiomagalhaes.com>
2020-09-04 09:55:44 -0300

    add tests to server status check

--- spec/models/server_status_check_spec.rb
+++ spec/models/server_status_check_spec.rb
@@ -11,5 +11,7 @@
 require 'rails_helper'
 
 RSpec.describe ServerStatusCheck, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:server) }
+  end
 end
