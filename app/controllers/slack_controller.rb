bcf9e8f2179e2db61d7647b60b3b4e4fac8a4f34
kaiomagalhaes <me@kaiomagalhaes.com>
2020-09-04 10:09:35 -0300

    remove slack controller

--- app/controllers/slack_controller.rb
+++
@@ -1,9 +0,0 @@
-class SlackController < ApplicationController
-  def index
-    repository = Repository.where(name: params[:repository_name]).first
-    pull_request = PullRequest.where(github_id: params[:github_id], repository: repository).last
-    @slack_message = SlackMessage.where(pull_request: pull_request).first
-
-    render json: @slack_message
-  end
-end
