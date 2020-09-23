2cfa8a7de52f1f879094a258c2abbd0534b1e29b
Rheniery <rhenierymendes@gmail.com>
2020-09-23 15:33:21 -0300

    Update slack message of the sentry's notification

--- app/services/parsers/sentry_webhook_parser.rb
+++ app/services/parsers/sentry_webhook_parser.rb
@@ -1,16 +1,19 @@
 # frozen_string_literal: true
 
+require 'ostruct'
 module Parsers
   class SentryWebhookParser < BaseParser
-    attr_reader :title, :project_name
+    delegate :contexts, :id, :location, :metadata, :project, :title, :user, to: :event, prefix: true, allow_nil: true
+    attr_reader :event, :issue_id, :project_name
 
     def can_parse?
       @json && @json[:project_name] && @json[:event]
     end
 
     def parse!
-      @title = @json.dig(:event, :title)
       @project_name = @json[:project_name]
+      @event = OpenStruct.new @json[:event]
+      @issue_id = @json[:id]
     end
   end
 end
