7062d56be87efed16d01bb73beca06946677b8f5
Rheniery Mendes <rhenierymendes@gmail.com>
2020-10-08 10:53:32 -0300

    Revert "Fix migrations from the server incident model"
    This reverts commit dc7a701b14a3b53d83cbfd3db0db3290045cc0ed.

--- db/data/20200925173702_update_status_to_server_incidents.rb
+++ db/data/20200925173702_update_status_to_server_incidents.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-class UpdateStateToServerIncidents < ActiveRecord::Migration[6.0]
+class UpdateStatusToServerIncidents < ActiveRecord::Migration[6.0]
   def up
     ServerIncident.find_each do |s|
-      s.update(state: 'completed')
+      s.update(status: 'completed')
     end
   end
 
