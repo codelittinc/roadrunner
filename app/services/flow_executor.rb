a418ec1aad79c70289de12e7b2906bad2e832f7b
Rheniery <rhenierymendes@gmail.com>
2020-10-14 09:54:03 -0300

    Send messages in case of exceptions in flows

--- app/services/flow_executor.rb
+++ app/services/flow_executor.rb
@@ -6,23 +6,16 @@ class FlowExecutor
   end
 
   def execute
-    files = Dir['./app/services/flows/*'].reject do |file|
-      file.include?('base_flow')
-    end
-
-    classnames = files.map do |file|
-      regex = %r{/([a-z_]+).rb}
-      file.match(regex)[1].split('_').map(&:capitalize).join if file.match?(regex)
-    end.compact
-
+    executed = true
     flow_request = FlowRequest.create!(json: @params.to_json)
 
     classnames.each do |classname|
       classConst = Object.const_get("Flows::#{classname}")
       object = classConst.new(@params)
-
+      executed = false
       next unless object.flow?
 
+      executed = true
       flow_request.update(flow_name: object.class.name)
 
       begin
@@ -31,8 +24,58 @@ class FlowExecutor
       rescue Exception => e
         message = [e.to_s, e.backtrace].flatten.join("\n")
         flow_request.update(error_message: message)
+        send_exception_message! if channel_name
         raise e
       end
+
+      break
     end
+
+    send_no_result_message! unless executed
+  end
+
+  private
+
+  def files
+    @files ||= Dir['./app/services/flows/*'].reject do |file|
+      file.include?('base_flow')
+    end
+  end
+
+  def classnames
+    @classnames ||= files.map do |file|
+      regex = %r{/([a-z_]+).rb}
+      file.match(regex)[1].split('_').map(&:capitalize).join if file.match?(regex)
+    end.compact
+  end
+
+  def send_channel_message_result(message, channel)
+    Clients::Slack::ChannelMessage.new.send(message, channel)
+  end
+
+  def send_direct_message_result(message, username)
+    Clients::Slack::DirectMessage.new.send(message, username)
+  end
+
+  def channel_name
+    @channel_name ||= @params[:channel_name]
+  end
+
+  def user_name
+    @user_name ||= @params[:user_name]
+  end
+
+  def send_no_result_message!
+    message = Messages::Builder.notify_no_results_from_flow
+    if channel_name
+      send_channel_message_result(message, channel_name)
+    elsif user_name
+      send_direct_message_result(message, user_name)
+    end
+  end
+
+  def send_exception_message!
+    message = Messages::Builder.notify_exception_from_flow
+    send_channel_message_result(message, channel_name)
   end
 end
---
+++ spec/services/flow_executor_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe FlowExecutor, type: :service do
+  describe '#execute' do
+    context 'when there is an exception' do
+      it 'sends an exception message' do
+        VCR.use_cassette('services#flowexecutor#handle-exception') do
+          repository = FactoryBot.create(:repository)
+          repository.slack_repository_info.update(deploy_channel: 'feed-test-automations')
+          # branch from the json doesn't exist
+          flow_executor = described_class.new({
+                                                "text": 'hotfix qa roadrunner-repository-test hotfix/test-hotfix-octtefdhh',
+                                                "channel_name": 'feed-test-automations',
+                                                "user_name": 'rheniery.mendes'
+                                              })
+
+          expected_message = 'There was an error with your request. Hey @automations-dev can you please check this?'
+          allow_any_instance_of(Clients::Slack::ChannelMessage).to receive(:send)
+          allow_any_instance_of(Clients::Slack::ChannelMessage).to receive(:send).with(expected_message, 'feed-test-automations')
+
+          expect { flow_executor.execute }.to change { FlowRequest.count }.by(1)
+        end
+      end
+    end
+
+    context 'when there is no results from flows and the command was sent through Channel' do
+      it 'sends a channel no results message' do
+        flow_executor = described_class.new({
+                                              "text": 'test',
+                                              "channel_name": 'feed-test-automations',
+                                              "user_name": 'rheniery.mendes'
+                                            })
+
+        expected_message = 'There are no results for your request. Please, check for more information using the `/roadrunner help` command.'
+        expect_any_instance_of(Clients::Slack::ChannelMessage).to receive(:send).with(expected_message, 'feed-test-automations')
+
+        expect { flow_executor.execute }.to change { FlowRequest.count }.by(1)
+      end
+    end
+    context 'when there is no results from flows and the command was sent through Direct Message' do
+      it 'sends a direct no results message' do
+        flow_executor = described_class.new({
+                                              "text": 'test',
+                                              "user_name": 'rheniery.mendes'
+                                            })
+
+        expected_message = 'There are no results for your request. Please, check for more information using the `/roadrunner help` command.'
+        expect_any_instance_of(Clients::Slack::DirectMessage).to receive(:send).with(expected_message, 'rheniery.mendes')
+
+        expect { flow_executor.execute }.to change { FlowRequest.count }.by(1)
+      end
+    end
+  end
+end
