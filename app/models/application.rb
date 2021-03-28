1e7f8434ab52282e0058a7d7bc585c5fd65cf522
“Vinícius <vini89go@gmail.com>
2021-03-28 04:15:33 -0300

    Add application model

---
+++ app/models/application.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: application
+#
+#  id                  :bigint         not null, primary key
+#  environment         :string
+#  version             :string
+#  external_identifier :string
+#  repository_id       :bigint         not null
+#  created_at          :datetime       not null
+#  updated_at          :datetime       not null
+#
+class Application < ApplicationRecord
+  belongs_to :repository
+  has_one :server
+
+  validates :environment, presence: true
+  validates :version, presence: true
+  validates :external_identifier, presence: true, uniqueness: true
+end
