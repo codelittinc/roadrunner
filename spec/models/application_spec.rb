94d6c4d682c6794f540c49d7775b92b0d0c28dca
“Vinícius <vini89go@gmail.com>
2021-03-28 04:19:29 -0300

    Added application model spec

---
+++ spec/models/application_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: application
+#
+#  id                  :bigint         not null, primary key
+#  environment         :string
+#  version             :string
+#  external_identifier :string
+#  repository_id       :bigint         not null
+#  created_at          :datetime       not null
+#  updated_at          :datetime       not null
+#
+require 'rails_helper'
+
+RSpec.describe Application, type: :model do
+  describe 'associations' do
+    it { should belong_to(:repository) }
+    it { should have_one(:server) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:environment) }
+    it { should validate_presence_of(:version) }
+    it { should validate_presence_of(:external_identifier) }
+    it { should validate_uniqueness_of(:external_identifier) }
+  end
+end
