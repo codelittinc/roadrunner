288d4d67fb9bdd53ea1c22958d71247ea9dd0d1b
“Vinícius <vini89go@gmail.com>
2021-04-03 01:51:58 -0300

    Add 'repositories' 'create' route. Add 'repositories' controller.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :applications, only: :show do
     resources :changelogs, only: :index
   end
+  resources :repositories, only: :create
 
   root 'application#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
