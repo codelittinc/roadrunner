b6b36b075572ab479b16b017bd726c385fd742bf
“Vinícius <vini89go@gmail.com>
2021-04-16 10:39:49 -0300

    'changelogs' service now displays the correct commits messages

--- app/services/changelogs_service.rb
+++ app/services/changelogs_service.rb
@@ -5,27 +5,17 @@ class ChangelogsService
     @application = application
   end
 
+  def commits_data
+    commits = @application.latest_release.commits
+    commits.map do |commit|
+      { message: commit.message }
+    end
+  end
+
   def changelog
-    [
-      {
-        version: '1.0.0',
-        changes: [
-          {
-            message: 'Create form component',
-            references: [
-              'https://codelitt.atlassian.net/secure/RapidBoard.jspa?rapidView=35&projectKey=ADS&modal=detail&selectedIssue=ADS-68',
-              'https://codelitt.atlassian.net/secure/RapidBoard.jspa?rapidView=35&projectKey=ADS&modal=detail&selectedIssue=ADS-69'
-            ]
-          },
-          {
-            message: 'Create input component',
-            references: [
-              'https://codelitt.atlassian.net/secure/RapidBoard.jspa?rapidView=35&projectKey=ADS&modal=detail&selectedIssue=ADS-66',
-              'https://codelitt.atlassian.net/secure/RapidBoard.jspa?rapidView=35&projectKey=ADS&modal=detail&selectedIssue=ADS-69'
-            ]
-          }
-        ]
-      }
-    ]
+    {
+      version: @application.latest_release.version,
+      changes: commits_data
+    }
   end
 end
