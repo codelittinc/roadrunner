cfbd21eb15024df33539ea70db5c87eb9c9e8f69
kaiomagalhaes <me@kaiomagalhaes.com>
2021-04-21 20:21:11 -0300

    Revert "remove has many through from project due to rails admin error"
    This reverts commit 5d94a8a73b7ba620d1f79e684c2f34a793c8b010.

--- app/models/project.rb
+++ app/models/project.rb
@@ -15,4 +15,6 @@ class Project < ApplicationRecord
   friendly_id :name, use: :slugged
 
   has_many :repositories
+  has_many :servers, through: :repositories
+  has_many :applications, through: :repositories
 end
