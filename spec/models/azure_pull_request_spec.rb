2dec46e439fa653bc54619544673cc9e0bb3b18c
“Vinícius <vini89go@gmail.com>
2021-04-28 10:53:17 -0300

    Add azure pull request and respective tests.

---
+++ spec/models/azure_pull_request_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: azure_pull_requests
+#
+#  id              :bigint           not null, primary key
+#  azure_id        :string
+#  pull_request_id :bigint
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+require 'rails_helper'
+
+RSpec.describe AzurePullRequest, type: :model do
+  describe 'associations' do
+    it { should belong_to(:pull_request) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:azure_id) }
+    it { should validate_presence_of(:pull_request) }
+  end
+end
