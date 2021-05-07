adab47328a3a2fe5b0d2e7a795515d4f049301a1
Rheniery <rhenierymendes@gmail.com>
2021-05-07 17:17:02 -0300

    Add Azure commits compare request

--- spec/services/clients/azure/branch_spec.rb
+++ spec/services/clients/azure/branch_spec.rb
@@ -12,4 +12,29 @@ RSpec.describe Clients::Azure::Branch, type: :service do
       end
     end
   end
+
+  describe '#compare' do
+    it 'returns a list the commits difference between two branches' do
+      VCR.use_cassette('azure#branch#compare') do
+        commits = described_class.new.compare('ay-users-api-test', 'main', 'feat/test')
+        expect(commits.size).to eql(4)
+      end
+    end
+  end
+
+  describe '#branch_exists' do
+    it 'returns true when the branch exists' do
+      VCR.use_cassette('azure#branch#branch_exists_true') do
+        exists = described_class.new.branch_exists?('ay-users-api-test', 'roadrunner/test')
+        expect(exists).to be_truthy
+      end
+    end
+
+    it 'returns true when the branch exists' do
+      VCR.use_cassette('azure#branch#branch_exists_false') do
+        exists = described_class.new.branch_exists?('ay-users-api-test', 'roadrunner/test1234')
+        expect(exists).to be_falsey
+      end
+    end
+  end
 end
