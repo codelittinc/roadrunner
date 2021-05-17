c5be3771e3585b1dab70b7a9c4629ea59a98a93f
“Vinícius <vini89go@gmail.com>
2021-05-17 14:52:36 -0300

    Add azure repository client and repository parser

---
+++ app/services/clients/azure/repository.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Clients
+  module Azure
+    class Repository < AzureBase
+      def get_repository(repository)
+        url = "#{azure_url}git/repositories/#{repository.name}?includeParent=false&api-version=4.1"
+        repository = Request.get(url, authorization)
+        Clients::Azure::Parsers::RepositoryParser.new(repository)
+      end
+    end
+  end
+end
