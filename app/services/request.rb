6f82feb3127a6c06e3ac0f1ac229fdd2668876bd
kaiomagalhaes <me@kaiomagalhaes.com>
2021-05-24 20:19:23 -0300

    allow to make non ssl requests

--- app/services/request.rb
+++ app/services/request.rb
@@ -1,21 +1,23 @@
 # frozen_string_literal: true
 
 require 'net/http'
+require 'json'
 
 class Request
   def self.get(url, authorization = nil)
-    url = URI.parse(url)
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    req = Net::HTTP::Get.new(url.request_uri)
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = use_ssl?(url)
+    req = Net::HTTP::Get.new(uri.request_uri)
     req['Authorization'] = authorization
     JSON.parse(http.request(req).body)
   end
 
   def self.post(url, authorization, body)
+    use_ssl = url.match?(/^https/)
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
+    http.use_ssl = use_ssl?(url)
 
     request = Net::HTTP::Post.new(uri.path, { 'Content-Type' => 'application/json' })
     request.body = body.to_json
@@ -28,7 +30,7 @@ class Request
   def self.patch(url, authorization, body)
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
+    http.use_ssl = use_ssl?(url)
 
     request = Net::HTTP::Patch.new(uri.path, { 'Content-Type' => 'application/json' })
     request.body = body.to_json
@@ -36,5 +38,9 @@ class Request
     request['Accept'] = 'application/json'
 
     response = http.request(request)
+  end
+
+  def self.use_ssl?(url)
+    url.match?(/^https/)
   end
 end
