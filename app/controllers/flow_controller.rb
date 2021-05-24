04888cc7b20766ab336f2aaedd090c5dd197e9ad
kaiomagalhaes <me@kaiomagalhaes.com>
2021-05-24 20:29:46 -0300

    Revert "execute the flow execution in a thread to avoid timeout issues with slack"
    This reverts commit 620ec7e2cdfdfca8c5c830b5b413e948d71a50aa.

--- app/controllers/flow_controller.rb
+++ app/controllers/flow_controller.rb
@@ -2,12 +2,8 @@
 
 class FlowController < ApplicationController
   def create
-    Thread.new do
-      Rails.application.executor.wrap do
-        FlowExecutor.new((params[:flow] || params).merge(request.GET)).execute
-      end
-    end
-
     render json: { text: 'Roadrunner is processing your request.' }, status: :ok
+
+    FlowExecutor.new((params[:flow] || params).merge(request.GET)).execute
   end
 end
