11af835159fa6571f899bcee18d7f5fb934fc532
kaiomagalhaes <me@kaiomagalhaes.com>
2021-05-25 11:17:05 -0300

    fix azure compare branchs

--- spec/services/clients/azure/branch_spec.rb
+++ spec/services/clients/azure/branch_spec.rb
@@ -18,11 +18,30 @@ RSpec.describe Clients::Azure::Branch, type: :service do
   end
 
   describe '#compare' do
-    # @TODO: we need to add an example that brings more than one commit item
-    it 'returns a list the commits difference between two branches' do
+    it 'returns the correct commits between two branches' do
+      repository = FactoryBot.create(:repository, name: 'roadrunner-repository-test')
       VCR.use_cassette('azure#branch#compare') do
-        commits = described_class.new.compare(repository, 'master', 'feat/cool-test')
-        expect(commits.size).to eql(1)
+        commits = described_class.new.compare(repository, 'master', 'feat/test-compare-branches')
+        expected_commits_sha = %w[
+          cc6c48dae95743d3dbcdbf8c58dc4bf6c8ee120f
+          8773fb97bbbeee26a0b0a70a6ddecc39bc1226c9
+          94047697dd6287f851d26ebf7236f128735e81c3
+        ]
+        expect(commits.map(&:sha)).to eql(expected_commits_sha)
+      end
+    end
+
+    it 'returns the correct commits between two tags' do
+      repository = FactoryBot.create(:repository, name: 'roadrunner-repository-test')
+      VCR.use_cassette('azure#branch#compare') do
+        commits = described_class.new.compare(repository, 'rc.1.v1.1.0', 'v1.0.0')
+        expected_commits_sha = %w[
+          cc6c48dae95743d3dbcdbf8c58dc4bf6c8ee120f
+          8773fb97bbbeee26a0b0a70a6ddecc39bc1226c9
+          94047697dd6287f851d26ebf7236f128735e81c3
+          5964d2ba6159d6c0cdc7c5241f59f4a6d7d8dd3e
+        ]
+        expect(commits.map(&:sha)).to eql(expected_commits_sha)
       end
     end
   end
