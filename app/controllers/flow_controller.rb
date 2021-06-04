94decef6ed63e558e800394df72efc210196d033
kaiomagalhaes <me@kaiomagalhaes.com>
2021-06-04 10:19:37 -0300

    Revert "add sidekiq to handle the flow executions"
    This reverts commit e7c8f22cf4f793a57242f3b5bc343d9c9491a014.

--- app/controllers/flow_controller.rb
+++ app/controllers/flow_controller.rb
@@ -2,11 +2,8 @@
 
 class FlowController < ApplicationController
   def create
-    json = (params[:flow] || params).merge(request.GET).to_json
-    flow_request = FlowRequest.create!(json: json)
-
-    HardWorker.perform_async(flow_request.id)
-
     render json: { text: 'Roadrunner is processing your request.' }, status: :ok
+
+    FlowExecutor.new((params[:flow] || params).merge(request.GET)).execute
   end
 end
--- spec/services/flow_executor_spec.rb
+++ spec/services/flow_executor_spec.rb
@@ -10,48 +10,46 @@ RSpec.describe FlowExecutor, type: :service do
           repository = FactoryBot.create(:repository)
           repository.slack_repository_info.update(deploy_channel: 'feed-test-automations')
           # branch from the json doesn't exist
-          flow_request = FlowRequest.create!(json: {
-            text: 'hotfix qa roadrunner-repository-test hotfix/test-hotfix-octtefdhh',
-            channel_name: 'feed-test-automations',
-            user_name: 'rheniery.mendes'
-          }.to_json)
-          flow_executor = described_class.new(flow_request)
+          flow_executor = described_class.new({
+                                                text: 'hotfix qa roadrunner-repository-test hotfix/test-hotfix-octtefdhh',
+                                                channel_name: 'feed-test-automations',
+                                                user_name: 'rheniery.mendes'
+                                              })
 
           expected_message = 'There was an error with your request. Hey @automations-dev can you please check this?'
           allow_any_instance_of(Clients::Slack::ChannelMessage).to receive(:send)
           allow_any_instance_of(Clients::Slack::ChannelMessage).to receive(:send).with(expected_message, 'feed-test-automations')
-          flow_executor.execute!
+
+          expect { flow_executor.execute }.to change { FlowRequest.count }.by(1)
         end
       end
     end
 
     context 'when there is no results from flows and the command was sent through Channel' do
       it 'sends a channel no results message' do
-        flow_request = FlowRequest.create!(json: {
-          text: 'hotfix qa roadrunner-repository-test hotfix/test-hotfix-octtefdhh',
-          channel_name: 'feed-test-automations',
-          user_name: 'rheniery.mendes'
-        }.to_json)
-        flow_executor = described_class.new(flow_request)
+        flow_executor = described_class.new({
+                                              text: 'test',
+                                              channel_name: 'feed-test-automations',
+                                              user_name: 'rheniery.mendes'
+                                            })
 
         expected_message = 'There are no results for your request. Please, check for more information using the `/roadrunner help` command.'
         expect_any_instance_of(Clients::Slack::DirectMessage).to receive(:send).with(expected_message, 'rheniery.mendes')
 
-        flow_executor.execute!
+        expect { flow_executor.execute }.to change { FlowRequest.count }.by(1)
       end
     end
     context 'when there is no results from flows and the command was sent through Direct Message' do
       it 'sends a direct no results message' do
-        flow_request = FlowRequest.create!(json: {
-          text: 'test',
-          user_name: 'rheniery.mendes'
-        }.to_json)
-        flow_executor = described_class.new(flow_request)
+        flow_executor = described_class.new({
+                                              text: 'test',
+                                              user_name: 'rheniery.mendes'
+                                            })
 
         expected_message = 'There are no results for your request. Please, check for more information using the `/roadrunner help` command.'
         expect_any_instance_of(Clients::Slack::DirectMessage).to receive(:send).with(expected_message, 'rheniery.mendes')
 
-        flow_executor.execute!
+        expect { flow_executor.execute }.to change { FlowRequest.count }.by(1)
       end
     end
   end
