69f7322133793d92a8fa95f0ce23e135aeea2be7
kaiomagalhaes <me@kaiomagalhaes.com>
2021-06-14 10:27:22 -0300

    fix create repository service

---
+++ app/services/clients/azure/hook.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Clients
+  module Azure
+    class Hook < AzureBase
+      # @TODO: implement this method
+      def list(_repository); end
+
+      # @TODO: implement this method
+      def create(repository, name = 'web'); end
+    end
+  end
+end
