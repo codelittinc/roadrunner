cb874f8b2284df271d8a312f1c611e5a47df9914
Vinicius <vini89go@gmail.com>
2021-06-17 14:48:32 -0300

    Updated slack client urls to use underscore instead of dash

--- app/services/clients/slack/direct_message.rb
+++ app/services/clients/slack/direct_message.rb
@@ -4,19 +4,10 @@ module Clients
   module Slack
     class DirectMessage < BaseSlack
       def send(message, username)
-        url = build_url('/direct-messages')
+        url = build_url('/direct_messages')
         Request.post(url, authorization, build_params({
                                                         message: message,
                                                         username: username
-                                                      }))
-      end
-
-      def send_ephemeral(blocks, username, channel = nil)
-        url = build_url('/direct-ephemeral-messages')
-        Request.post(url, authorization, build_params({
-                                                        blocks: blocks,
-                                                        username: username,
-                                                        channel: channel
                                                       }))
       end
     end
