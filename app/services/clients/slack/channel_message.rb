b3e1a023d2a2d21eb2abb0633f9529a575ae40fb
Vinicius <vini89go@gmail.com>
2021-06-17 14:48:32 -0300

    Updated slack client urls to use underscore instead of dash

--- app/services/clients/slack/channel_message.rb
+++ app/services/clients/slack/channel_message.rb
@@ -4,7 +4,7 @@ module Clients
   module Slack
     class ChannelMessage < BaseSlack
       def send(message, channel, timestamp = nil)
-        url = build_url('/channel-messages')
+        url = build_url('/channel_messages')
         response = Request.post(url, authorization, build_params({
                                                                    message: message,
                                                                    channel: channel,
@@ -14,7 +14,7 @@ module Clients
       end
 
       def update(message, channel, timestamp)
-        url = build_url('/channel-messages')
+        url = build_url('/channel_messages')
         response = Request.patch(url, authorization, build_params({
                                                                     message: message,
                                                                     channel: channel,
