b1f05ec3d93793e9ee8b6fb06ab5c0762bc485e4
kaiomagalhaes <me@kaiomagalhaes.com>
2021-06-24 16:26:37 -0300

    update sidekiq concurrency to 1 to avoid race conditions

--- bin/pull_pro_backup.sh
+++ bin/pull_pro_backup.sh
@@ -1,9 +1,9 @@
 rm latest.dump
-docker-compose -f docker-compose.development.yml up -d
-docker stop roadrunner-project-api
+docker-compose up -d
+docker stop roadrunner-api
 heroku pg:backups:capture --app prod-roadrunner
 heroku pg:backups:download --app prod-roadrunner
-docker exec -it roadrunner-project-db psql -U postgres -c 'DROP DATABASE IF EXISTS roadrunner_development'
-docker exec -it roadrunner-project-db psql -U postgres -c "CREATE DATABASE roadrunner_development"
-docker exec -it roadrunner-project-db pg_restore --no-owner  -U postgres -d roadrunner_development -1 ./share/latest.dump
+docker exec -it roadrunner-db psql -U postgres -c 'DROP DATABASE IF EXISTS roadrunner_development'
+docker exec -it roadrunner-db psql -U postgres -c "CREATE DATABASE roadrunner_development"
+docker exec -it roadrunner-db pg_restore --no-owner  -U postgres -d roadrunner_development -1 ./share/latest.dump
 sh bin/dev
