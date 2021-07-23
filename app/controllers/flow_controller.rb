e32702d18e26ce7a21575902d21858d83baa30e8
kaiomagalhaes <me@kaiomagalhaes.com>
2021-07-23 15:26:53 -0300

    fix sidekiq on flow controller

--- app/controllers/flow_controller.rb
+++ app/controllers/flow_controller.rb
@@ -5,7 +5,7 @@ class FlowController < ApplicationController
     json = (params[:flow] || params).merge(request.GET).to_json
     flow_request = FlowRequest.create!(json: json)
 
-    FlowExecutor.new(flow_request).execute! if flow_request
+    HardWorker.perform_async(flow_request.id)
 
     render json: { text: 'Roadrunner is processing your request.' }, status: :ok
   end
