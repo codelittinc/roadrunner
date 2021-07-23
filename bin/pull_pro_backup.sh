c474111f14ac96994fbcba92c135b33c030891b3
kaiomagalhaes <me@kaiomagalhaes.com>
2021-07-23 16:57:41 -0300

    unity Dockerfile and Dockeerfile.develeopment

--- bin/pull_pro_backup.sh
+++ bin/pull_pro_backup.sh
@@ -5,5 +5,5 @@ heroku pg:backups:capture --app prod-roadrunner
 heroku pg:backups:download --app prod-roadrunner
 docker exec -it roadrunner-db psql -U postgres -c 'DROP DATABASE IF EXISTS roadrunner_development'
 docker exec -it roadrunner-db psql -U postgres -c "CREATE DATABASE roadrunner_development"
-docker exec -it roadrunner-db pg_restore --no-owner  -U postgres -d roadrunner_development -1 ./share/latest.dump
+docker exec -it roadrunner-db pg_restore --no-owner  -U postgres -d roadrunner_development -1 ./app/latest.dump
 sh bin/dev
