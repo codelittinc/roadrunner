113fb07d05cd68b6cfb5285f85cbc6c282c1ed0d
kaiomagalhaes <me@kaiomagalhaes.com>
2021-08-23 10:42:48 -0300

    stop requiring author_email when creating an email

--- spec/models/commit_spec.rb
+++ spec/models/commit_spec.rb
@@ -19,7 +19,6 @@ RSpec.describe Commit, type: :model do
   describe 'should validate the props' do
     it { is_expected.to validate_presence_of(:sha) }
     it { is_expected.to validate_presence_of(:author_name) }
-    it { is_expected.to validate_presence_of(:author_email) }
   end
 
   describe 'associations' do
