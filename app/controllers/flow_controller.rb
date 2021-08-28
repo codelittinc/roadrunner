dac1d377c09a929e4da32ed0cc33216f75513d59
kaiomagalhaes <me@kaiomagalhaes.com>
2021-08-27 22:20:46 -0300

    fix branch name regex for Azure flows

--- app/controllers/flow_controller.rb
+++ app/controllers/flow_controller.rb
@@ -5,7 +5,11 @@ class FlowController < ApplicationController
     json = (params[:flow] || params).merge(request.GET).to_json
     flow_request = FlowRequest.create!(json: json)
 
-    HardWorker.perform_async(flow_request.id)
+    if Rails.env.production?
+      HardWorker.perform_async(flow_request.id)
+    else
+      FlowExecutor.new(flow_request).execute!
+    end
 
     render json: { text: 'Roadrunner is processing your request.' }, status: :ok
   end
