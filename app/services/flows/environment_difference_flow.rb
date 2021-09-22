af546aa77453cef35d6534782f2f1bc57e8f7373
raphaelsattler <raphaelsattler@hotmail.com>
2021-09-22 18:29:14 -0300

    Create EnvironmentDifference Flow

---
+++ app/services/flows/environment_difference_flow.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Flows
+  class EnvironmentDifferenceFlow < BaseFlow
+    def execute; end
+
+    def can_execute?
+      text.split.size == 5 &&
+        text.include?('env diff') &&
+        repository.present? &&
+        head_env.present? &&
+        base_env.present?
+    end
+
+    private
+
+    def text
+      @text ||= @params[:text]
+    end
+
+    def repository
+      repository_name = text.split.third
+      Repository.find_by(name: repository_name)
+    end
+
+    def base_env
+      base_env_name = text.split.fourth
+      repository.applications.find_by(environment: base_env_name)
+    end
+
+    def head_env
+      head_env_name = text.split.last
+      repository.applications.find_by(environment: head_env_name)
+    end
+  end
+end
