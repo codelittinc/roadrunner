74fbb5ef4eedfd69ec9eb84e627cf0b20bbbd734
raphaelsattler <raphaelsattler@hotmail.com>
2021-10-01 14:09:19 -0300

    Improve verifications on can_execute? on ReleaseDiff flow

--- spec/services/flows/release_difference_flow_spec.rb
+++ spec/services/flows/release_difference_flow_spec.rb
@@ -5,12 +5,22 @@ require 'rails_helper'
 RSpec.describe Flows::ReleaseDifferenceFlow, type: :service do
   let(:repository) { FactoryBot.create(:repository) }
   let(:application) { FactoryBot.create(:application, repository: repository) }
+  let(:base_release) { FactoryBot.create(:release, application: application) }
+  let(:head_release) { FactoryBot.create(:release, application: application) }
+
+  let(:commits) do
+    FactoryBot.create(:commit, :with_pull_request,
+                      message: 'commit on Base Release', release_ids: [base_release.id])
+    FactoryBot.create(:commit, :with_pull_request, message: 'commit on both releases', release_ids: [base_release.id, head_release.id])
+    FactoryBot.create(:commit, :with_pull_request, message: 'first commit on Head Release', release_ids: [head_release.id])
+    FactoryBot.create(:commit, :with_pull_request, message: 'second commit on Head Release', release_ids: [head_release.id])
+  end
 
   describe '#can_execute?' do
-    context 'with a valid json' do
-      it 'returns true' do
+    context 'returns true' do
+      it 'with a valid json' do
         flow = described_class.new({
-                                     text: 'release diff RepositoryName BaseRelease HeadRelease',
+                                     text: "release diff #{repository.name} #{base_release.version} #{head_release.version}",
                                      channel_name: 'cool-channel'
                                    })
         expect(flow.can_execute?).to be_truthy
@@ -32,45 +42,46 @@ RSpec.describe Flows::ReleaseDifferenceFlow, type: :service do
                                    })
         expect(flow.can_execute?).to be_falsey
       end
+
+      it 'an release does not exists' do
+        flow = described_class.new({
+                                     text: "env diff #{repository.name} #{base_release.version} fake_release_version",
+                                     channel_name: 'cool-channel'
+                                   })
+        expect(flow.can_execute?).to be_falsey
+      end
+
+      it 'a repository does not exists' do
+        flow = described_class.new({
+                                     text: "env diff fake_repository #{base_release} #{head_release}",
+                                     channel_name: 'cool-channel'
+                                   })
+        expect(flow.can_execute?).to be_falsey
+      end
+
+      it 'there are missing parameters' do
+        flow = described_class.new({
+                                     text: "env diff #{repository.name}",
+                                     channel_name: 'cool-channel'
+                                   })
+        expect(flow.can_execute?).to be_falsey
+      end
     end
   end
 
   describe '#execute' do
     context 'with a valid json' do
       it 'returns the commit list' do
-        base_release = FactoryBot.create(:release, application: application)
-        head_release = FactoryBot.create(:release, application: application)
-
-        FactoryBot.create(:commit, :with_pull_request,
-                          message: 'commit on Base Release', release_ids: [base_release.id])
-        FactoryBot.create(:commit, :with_pull_request, message: 'commit on both releases', release_ids: [base_release.id, head_release.id])
-        FactoryBot.create(:commit, :with_pull_request, message: 'first commit on Head Release', release_ids: [head_release.id])
-        FactoryBot.create(:commit, :with_pull_request, message: 'second commit on Head Release', release_ids: [head_release.id])
+        commits
 
         flow = described_class.new({
-                                     text: "release diff #{application.repository.name} #{base_release.version} #{head_release.version}",
+                                     text: "release diff #{repository.name} #{base_release.version} #{head_release.version}",
                                      channel_name: 'cool-channel',
                                      user_name: 'sattler'
                                    })
 
         expect_any_instance_of(Clients::Slack::DirectMessage).to receive(:send).with(
           "The differente between #{base_release.version} and #{head_release.version} is:\n - first commit on Head Release\n - second commit on Head Release", 'sattler'
-        )
-
-        flow.execute
-      end
-    end
-
-    context 'with a nonexistent release version' do
-      it 'returns a error message' do
-        flow = described_class.new({
-                                     text: "release diff #{application.repository.name} NonexistentBaseReleaseVersion NonexistentHeadReleaseVersion",
-                                     channel_name: 'cool-channel',
-                                     user_name: 'sattler'
-                                   })
-
-        expect_any_instance_of(Clients::Slack::DirectMessage).to receive(:send).with(
-          "I couldn't compare your releases. There is possibly a typo in their names or they are missing.", 'sattler'
         )
 
         flow.execute
