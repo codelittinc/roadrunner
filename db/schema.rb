02abea12696c551ab2209c7dcea3ff045a64aac4
kaiomagalhaes <me@kaiomagalhaes.com>
2021-10-15 01:17:14 -0300

    add team to sprint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_15_033224) do
+ActiveRecord::Schema.define(version: 2021_10_15_041523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -268,6 +268,7 @@ ActiveRecord::Schema.define(version: 2021_10_15_033224) do
     t.string "time_frame"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "team"
   end
 
   create_table "user_admins", force: :cascade do |t|
