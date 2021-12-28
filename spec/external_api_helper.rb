aba26600bc84d7e94426449c15c10a862767aff0
Kaio Magalhaes <me@kaiomagalhaes.com>
2021-12-27 22:34:10 -0300

    remove sensitive data for services

--- spec/external_api_helper.rb
+++ spec/external_api_helper.rb
@@ -4,4 +4,8 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.filter_sensitive_data('<SENSITIVE_DATA>') { ENV['GIT_AUTH_KEY'] }
+  config.filter_sensitive_data('<SENSITIVE_DATA>') { ENV['AZURE_AUTH_KEY'] }
+  config.filter_sensitive_data('<SENSITIVE_DATA>') { ENV['SLACK_API_KEY'] }
+  config.filter_sensitive_data('<SENSITIVE_DATA>') { ENV['JIRA_AUTH_KEY'] }
+  config.filter_sensitive_data('<SENSITIVE_DATA>') { ENV['NOTIFICATIONS_API_KEY'] }
 end
