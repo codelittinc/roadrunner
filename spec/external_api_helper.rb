678019a0db4318091fa9f2662bf0b9de273a19b1
Kaio Magalhaes <me@kaiomagalhaes.com>
2021-12-27 22:29:39 -0300

    clean github sensitive data

--- spec/external_api_helper.rb
+++ spec/external_api_helper.rb
@@ -3,4 +3,5 @@
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
+  config.filter_sensitive_data('<SENSITIVE_DATA>') { ENV['GIT_AUTH_KEY'] }
 end
