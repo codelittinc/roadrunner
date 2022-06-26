57145306dfe40de124d2553d66f947e0289e4660
Kaio Magalhaes <me@kaiomagalhaes.com>
2022-06-26 12:00:15 -0300

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery unless: -> { request.format.json? }
+
   def index
     render json: { status: 200 }
   end
