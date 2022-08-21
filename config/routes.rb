2f3201537b15512d773a406b83aff28c7f43b9cc
Kaio Magalhaes <me@kaiomagalhaes.com>
2022-08-21 14:18:19 +0200

    add basic scaffold for repositories view

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ require 'sidekiq/web'
 require 'sidekiq/cron/web'
 
 Rails.application.routes.draw do
+  resources :repositories
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount Sidekiq::Web => '/sidekiq'
 
