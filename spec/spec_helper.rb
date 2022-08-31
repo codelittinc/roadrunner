80cd02cabbb1605f0723295742e0b83f4707c5b4
Kaio Magalhaes <me@kaiomagalhaes.com>
2022-08-31 14:30:53 -0300

    Install and set up Buildkite Test Analytics

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,11 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require "buildkite/test_collector"
+
+Buildkite::TestCollector.configure(hook: :rspec)
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
