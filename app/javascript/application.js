b41a3a4dcb79e3817466e33c152735af0139c798
Kaio Magalhaes <me@kaiomagalhaes.com>
2022-08-31 18:55:05 -0300

    fix select load

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Load selects
+document.addEventListener("turbo:load", function (e) {
+    const elems = document.querySelectorAll('select');
+    M.FormSelect.init(elems);
+})
