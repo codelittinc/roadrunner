46aa5fb32ca8aba2a66385b5f6861c3812eee1f8
Kaio Magalhaes <me@kaiomagalhaes.com>
2022-10-30 11:53:04 -0300

    remove dev from hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,6 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.hosts << 'dev.api.roadrunner.codelitt.dev'
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
