f1c0e0d6de1e34116b1e375fdf8b0171fba750e2
Kaio Magalhaes <me@kaiomagalhaes.com>
2022-10-30 11:49:09 -0300

    add dev domain to the accepted hosts in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.hosts << 'dev.api.roadrunner.codelitt.dev'
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
