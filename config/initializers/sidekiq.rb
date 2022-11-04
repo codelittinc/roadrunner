e319595924b6e2cc43914e0cbc8a26dbc74ff5aa
Kaio Magalhaes <me@kaiomagalhaes.com>
2022-11-04 13:22:06 -0300

    remove sidekiq cron implementation

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,12 +2,6 @@
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', nil) }
-
-  config.on(:startup) do
-    schedule_file = 'config/schedule.yml'
-
-    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file)
-  end
 end
 
 Sidekiq.configure_client do |config|
