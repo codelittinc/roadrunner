79f8e87aae10bb196ffe594dfa694f1a1b7235d0
Kaio Magalhaes <me@kaiomagalhaes.com>
2022-11-25 12:47:02 -0300

    create flow builder

--- app/services/flow_executor.rb
+++ app/services/flow_executor.rb
@@ -7,59 +7,21 @@ class FlowExecutor
   end
 
   def execute!
-    executed = true
-
-    classnames.each do |classname|
-      classConst = Object.const_get("Flows::#{classname}")
-      object = classConst.new(@params)
-      executed = false
-      next unless object.flow?
-
-      executed = true
-      @flow_request.update(flow_name: object.class.name)
-
-      object.run
+    flow = FlowBuilder.build(@flow_request)
+    if flow
+      @flow_request.update(flow_name: flow.class.name)
+      flow.run
       @flow_request.update(executed: true)
-
-      break
+    else
+      send_no_result_message!
     end
-
-    send_no_result_message! unless executed
   end
 
   private
 
-  def files
-    @files ||= Dir['./app/services/flows/*'].reject do |file|
-      file.include?('base')
-    end
-  end
-
-  def classnames
-    @classnames ||= files.map do |file|
-      regex = %r{/([a-z_]+).rb}
-      file.match(regex)[1].split('_').map(&:capitalize).join if file.match?(regex)
-    end.compact
-  end
-
-  def send_channel_message_result(message, channel)
-    Clients::Notifications::Channel.new.send(message, channel)
-  end
-
-  def send_direct_message_result(message, username)
-    Clients::Notifications::Direct.new.send(message, username)
-  end
-
-  def channel_name
-    @channel_name ||= @params[:channel_name]
-  end
-
-  def user_name
-    @user_name ||= @params[:user_name]
-  end
-
   def send_no_result_message!
     message = Messages::GenericBuilder.notify_no_results_from_flow(@params[:text])
-    send_direct_message_result(message, user_name) if user_name
+    username = @params[:user_name]
+    Clients::Notifications::Direct.new.send(message, username) if username
   end
 end
--- spec/services/flow_executor_spec.rb
+++ spec/services/flow_executor_spec.rb
@@ -4,46 +4,23 @@ require 'rails_helper'
 
 RSpec.describe FlowExecutor, type: :service do
   describe '#execute' do
-    context 'when there is an exception' do
-      it 'sends an exception message' do
-        VCR.use_cassette('services#flowexecutor#handle-exception') do
-          repository = FactoryBot.create(:repository)
-          repository.slack_repository_info.update(deploy_channel: 'feed-test-automations')
-          # branch from the json doesn't exist
-          flow_request = FlowRequest.create!(json: {
-            text: 'hotfix qa roadrunner-repository-test hotfix/test-hotfix-octtefdhh',
-            channel_name: 'feed-test-automations',
-            user_name: 'rheniery.mendes'
-          }.to_json)
-          flow_executor = described_class.new(flow_request)
-
-          expected_message = 'There was an error with your request. Hey @automations-dev can you please check this?'
-          allow_any_instance_of(Clients::Notifications::Channel).to receive(:send)
-          allow_any_instance_of(Clients::Notifications::Channel).to receive(:send).with(expected_message,
-                                                                                        'feed-test-automations')
-          flow_executor.execute!
-        end
-      end
-    end
-
-    context 'when there is no results from flows and the command was sent through Channel' do
-      it 'sends a channel no results message' do
-        flow_request_text = 'hotfix qa roadrunner-repository-test hotfix/test-hotfix-octtefdhh'
+    context 'when the is a result' do
+      it 'executes the flow' do
+        flow_request_text = 'help'
         flow_request = FlowRequest.create!(json: {
           text: flow_request_text,
-          channel_name: 'feed-test-automations',
           user_name: 'rheniery.mendes'
         }.to_json)
+
         flow_executor = described_class.new(flow_request)
 
-        expected_message = "There are no results for *#{flow_request_text}*. Please, check for more information using the `/roadrunner help` command."
-        expect_any_instance_of(Clients::Notifications::Direct).to receive(:send).with(expected_message,
-                                                                                      'rheniery.mendes')
+        expect_any_instance_of(Flows::HelpSummaryFlow).to receive(:execute)
 
         flow_executor.execute!
       end
     end
-    context 'when there is no results from flows and the command was sent through Direct Message' do
+
+    context 'when there are no results' do
       it 'sends a direct no results message' do
         flow_request_text = 'test'
         flow_request = FlowRequest.create!(json: {
