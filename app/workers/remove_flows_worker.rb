d3b4202fc6aef64cfb17431370fa3d6e22fbc993
Alessandro <alessandromontividiu03@gmail.com>
2023-01-12 17:25:04 -0300

    Update remove_flows_worker.rb to not remove flows with an error message

--- app/workers/remove_flows_worker.rb
+++ app/workers/remove_flows_worker.rb
@@ -6,7 +6,7 @@ class RemoveFlowsWorker
   def perform(*_args)
     Rails.logger.info 'Removing flow requests older than 3 days ago...'
 
-    FlowRequest.where('created_at < ?', DateTime.now - 3.days).delete_all
+    FlowRequest.where('created_at < ? and error_message IS NULL', DateTime.now - 3.days).delete_all
 
     Rails.logger.info 'Flow requests removed successfully!'
   end
