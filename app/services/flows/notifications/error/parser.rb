9cbb8565ad7e6763b646c962eca66124dd6141ba
Alessandro <alessandromontividiu03@gmail.com>
2023-01-17 15:35:11 -0300

    Add Error flow to raise exceptions manually

---
+++ app/services/flows/notifications/error/parser.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Flows
+  module Notifications
+    module Error
+      class Parser < Parsers::BaseParser
+        attr_reader :name, :body
+
+        def can_parse?
+          @json[:name] == 'flow-raise-exception'
+        end
+
+        def parse!
+          @name = @json[:name].downcase
+          @body = @json[:body]
+        end
+      end
+    end
+  end
+end
