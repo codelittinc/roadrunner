5a830ca84bd1bc0f738fa0e2570f58d07c4611f8
Kaio Magalhães <me@kaiomagalhaes.com>
2023-02-21 14:36:04 +0000

    create projects views

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ require 'sidekiq/web'
 require 'sidekiq/cron/web'
 
 Rails.application.routes.draw do
+  resources :projects
   resources :organizations
   resources :repositories do
     resources :applications
