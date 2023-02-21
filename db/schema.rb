f28a1c619ff27516e9e13f318deb1ee00f2d0934
Kaio Magalhães <me@kaiomagalhaes.com>
2023-02-21 17:23:08 +0000

    add attr active to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_30_145657) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_21_171449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -147,6 +147,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_30_145657) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.bigint "customer_id"
+    t.boolean "active", default: true
     t.index ["customer_id"], name: "index_projects_on_customer_id"
     t.index ["slug"], name: "index_projects_on_slug", unique: true
   end
