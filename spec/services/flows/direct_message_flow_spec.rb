9ce86552f295e9922df5bc54d082c00a293c2528
Victor <vcarvalho0402@gmail.com>
2023-03-01 10:00:16 -0300

    feat: add flow to process direct messages to roadrunner

---
+++ spec/services/flows/direct_message_flow_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Flows::DirectMessageFlow, type: :service do
+  let(:direct_message_from_user) do
+    JSON.parse(File.read(File.join('spec', 'fixtures', 'services', 'flows',
+                                   'direct_message.json'))).with_indifferent_access
+  end
+
+  let(:direct_message_from_rr) do
+    JSON.parse(File.read(File.join('spec', 'fixtures', 'services', 'flows',
+                                   'direct_message_from_rr.json'))).with_indifferent_access
+  end
+
+  let(:message_with_rr_mention) do
+    JSON.parse(File.read(File.join('spec', 'fixtures', 'services', 'flows',
+                                   'message_mention_rr.json'))).with_indifferent_access
+  end
+
+  describe '#flow?' do
+    context 'when it is a message from a user' do
+      it 'returns true' do
+        flow = described_class.new(direct_message_from_user)
+        expect(flow.flow?).to be_truthy
+      end
+    end
+
+    context 'when it is a message from roadrunner' do
+      it 'returns false' do
+        flow = described_class.new(direct_message_from_rr)
+        expect(flow.flow?).to be_falsy
+      end
+    end
+
+    context 'when it is a message that mentions roadrunner' do
+      it 'returns true' do
+        flow = described_class.new(message_with_rr_mention)
+        expect(flow.flow?).to be_truthy
+      end
+    end
+  end
+
+  describe '#run' do
+    context 'when it is a direct message to roadrunner' do
+      it 'sends a message in the chat with roadrunner' do
+        flow = described_class.new(direct_message_from_user)
+        expect_any_instance_of(Clients::Notifications::Channel).to receive(:send).with(
+          'Yes, he is!',
+          'DSX1REERE'
+        )
+
+        flow.run
+      end
+    end
+
+    context 'when it is message in a channel that mentions roadrunner' do
+      it 'sends a message in the same channel' do
+        flow = described_class.new(message_with_rr_mention)
+        expect_any_instance_of(Clients::Notifications::Channel).to receive(:send).with(
+          'https://letmegpt.com?q=%20hello',
+          'C04SE02BGP2'
+        )
+
+        flow.run
+      end
+    end
+  end
+end
