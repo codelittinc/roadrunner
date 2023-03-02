c7ccf73a915e0f4d8cab09b4236bd61a31ce883a
Wilker Machado <wilker.machado@codelitt.com>
2023-03-02 09:18:01 -0300

    remove log for content from notion and update Gemfile.lock

--- app/workers/content_notion_worker.rb
+++ app/workers/content_notion_worker.rb
@@ -4,14 +4,12 @@ class ContentNotionWorker
   include Sidekiq::Worker
 
   def perform(*_args)
-    Rails.logger.info 'Updating notion content.'
+    Rails.logger.info 'Saving notion content.'
 
     content = Clients::NotionApi::Client.new.content
 
-    Rails.logger.debugger content
-
     ExternalResourceMetadata.new(key: 'notion-content', value: content).save!
 
-    Rails.logger.info 'Notion content updated successfully!'
+    Rails.logger.info 'Notion content saved successfully!'
   end
 end
