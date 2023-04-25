b69e589a42f274546bdcaf5389b03da54b6f3ee9
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-04-25 18:56:58 -0300

    update schema on pull_prod_deploy

--- bin/pull_pro_backup.sh
+++ bin/pull_pro_backup.sh
@@ -6,5 +6,6 @@ heroku pg:backups:capture --app prod-roadrunner
 heroku pg:backups:download --app prod-roadrunner
 docker exec -it roadrunner-db psql -U postgres -c 'DROP DATABASE IF EXISTS roadrunner_development'
 docker exec -it roadrunner-db psql -U postgres -c "CREATE DATABASE roadrunner_development"
+docker exec -it roadrunner-db psql -U postgres roadrunner_development -c "CREATE SCHEMA IF NOT EXISTS heroku_ext AUTHORIZATION postgres"
 docker exec -it roadrunner-db pg_restore --no-owner  -U postgres -d roadrunner_development -1 ./app/latest.dump
 sh bin/dev
