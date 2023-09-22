7040acf47f12d84f82f971e9a26aae45b5f4cb66
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-09-22 10:10:10 -0300

    add backstage to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost', 'localhost:3001'
+    origins 'localhost', 'localhost:3001', 'backstage.codelitt.dev'
 
     resource '*',
              headers: :any,
