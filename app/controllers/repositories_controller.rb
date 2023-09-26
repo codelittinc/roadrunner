740650112f026ca4de58ecb3a41aa1bc79d6f1dc
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-09-26 17:22:27 -0300

    clean up repository controller

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,60 +1,39 @@
 # frozen_string_literal: true
 
 class RepositoriesController < ApplicationController
-  before_action :set_repository, only: %i[show edit update destroy]
-  before_action :set_channels, only: %i[new edit]
-  before_action :clean_params, only: %i[create update]
+  before_action :set_repository, only: %i[show update destroy]
 
-  # GET /repositories or /repositories.json
   def index
     query = params[:query]
     @repositories = Repository.ransack(owner_cont: query, name_cont: query, m: 'or').result
     respond_to do |format|
-      format.html # index.html.erb
       format.json { render :index, formats: :json } # index.json.erb
     end
   end
 
-  # GET /repositories/1.json
   def show
     respond_to do |format|
-      format.html { redirect_to edit_repository_url(@repository.id) }
       format.json { render :show, formats: :json }
     end
   end
 
-  # GET /repositories/new
-  def new
-    @repository = Repository.new
-  end
-
-  # GET /repositories/1/edit
-  def edit; end
-
-  # POST /repositories or /repositories.json
   def create
     @repository = CreateRepositoryService.call(repository_params)
 
     respond_to do |format|
       if @repository.errors.empty?
-        format.html { redirect_to edit_repository_url(@repository.id), notice: 'Repository was successfully created.' }
         format.json { render :show, status: :created, location: @repository }
       else
-        set_channels
-        format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @repository.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /repositories/1 or /repositories/1.json
   def update
     respond_to do |format|
       if @repository.update(repository_params)
         format.json { render :show, status: :ok, location: @repository }
       else
-        set_channels
-        format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @repository.errors, status: :unprocessable_entity }
       end
     end
@@ -65,31 +44,17 @@ class RepositoriesController < ApplicationController
     @repository.destroy
 
     respond_to do |format|
-      format.html { redirect_to repositories_url, notice: 'Repository was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
 
-  def clean_params
-    params['repository']['deploy_type'] = nil if params['repository']['deploy_type'] == 'none'
-    params['repository']['deploy_type'] = Repository::TAG_DEPLOY_TYPE if params['repository']['deploy_type'].nil? || @repository&.deploy_type.nil?
-  end
-
-  # Use callbacks to share common setup or constraints between actions.
   def set_repository
     @repository = Repository.friendly.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def repository_params
     params.require(:repository).permit!
-  end
-
-  def set_channels
-    @channels = Clients::Notifications::Channel.new(Customer.find(1)).list.map do |c|
-      [c['name'], c['id']]
-    end
   end
 end
