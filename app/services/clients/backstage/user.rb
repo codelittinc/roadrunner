5b29f81d8c4b7c60e0f38723b49b79f68c3f5639
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-10-14 11:08:44 -0300

    update pull request direct comment to get users from backstage

--- app/services/clients/backstage/user.rb
+++ app/services/clients/backstage/user.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 class BackstageUser
-  attr_reader :email, :id
+  attr_reader :email, :id, :slack
 
   def initialize(params)
+    @params = params
     @email = params['email']
     @id = params['id']
+    @slack = identifier('slack')
+  end
+
+  def identifier(service_name)
+    @params['user_service_identifiers']&.find { |service| service['service_name'] == service_name }&.dig('identifier')
   end
 end
 
--- spec/services/clients/backstage/user_spec.rb
+++ spec/services/clients/backstage/user_spec.rb
@@ -12,6 +12,13 @@ RSpec.describe Clients::Backstage::User, type: :service do
           expect(users.map(&:email)).to eq(['kaio@codelitt.com', 'rheniery.mendes@codelitt.com'])
         end
       end
+
+      it 'returns a user that has a slack attribute' do
+        VCR.use_cassette('clients#backstage#clients#list') do
+          users = described_class.new.list(%w[azure-devops-kaio rony-azure-devops])
+          expect(users.map(&:slack)).to eq(['kaiomagalhaes', nil])
+        end
+      end
     end
   end
 end
