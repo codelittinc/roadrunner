9bbc7e1473e094a6d7d38009e36bdb4388b5280b
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-10-14 09:59:01 -0300

    create backstage user

---
+++ spec/services/clients/backstage/user_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'external_api_helper'
+
+RSpec.describe Clients::Backstage::User, type: :service do
+  describe '#show' do
+    context 'with a valid id' do
+      it 'returns the user' do
+        VCR.use_cassette('clients#backstage#clients#list') do
+          users = described_class.new.list(%w[azure-devops-kaio rony-azure-devops])
+          expect(users.map(&:email)).to eq(['kaio@codelitt.com', 'rheniery.mendes@codelitt.com'])
+        end
+      end
+    end
+  end
+end
