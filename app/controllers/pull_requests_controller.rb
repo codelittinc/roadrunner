2a6923d1b35b8a2c5b644358d34d38bd275bd9be
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-10-16 13:35:29 -0300

    fix pull requests users filter

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -2,15 +2,20 @@
 
 class PullRequestsController < ApplicationController
   def index
-    backstage_user_id = params[:backstage_user_id]
     start_date = params[:start_date]
     end_date = params[:end_date]
     state = params[:state]
-    project_id = params[:project_id]
 
-    @pull_requests = PullRequest.where(backstage_user_id:, created_at: start_date..end_date, state:)
+    @pull_requests = PullRequest.where(created_at: start_date..end_date, state:)
+
+    project_id = params[:project_id]
     return unless project_id
 
     @pull_requests = @pull_requests.joins(:repository).where(repository: { external_project_id: project_id })
+
+    backstage_user_id = params[:backstage_user_id]
+    return unless backstage_user_id
+
+    @pull_requests = @pull_requests.where(backstage_user_id:)
   end
 end
