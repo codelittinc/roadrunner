9a0aaa47147c74f4323a529c984225677ba46310
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-01-02 12:52:25 -0300

    update the code review flow to not create a new one in less than 5 minutes

--- spec/services/flows/repositories/pull_request/code_review/flow_spec.rb
+++ spec/services/flows/repositories/pull_request/code_review/flow_spec.rb
@@ -114,26 +114,6 @@ RSpec.describe Flows::Repositories::PullRequest::CodeReview::Flow, type: :servic
         end
       end
 
-      it 'sends the correct direct message' do
-        VCR.use_cassette('flows#new-review-submission-request#new-review-send-direct-message') do
-          user = FactoryBot.create(:user, slack: 'rheniery.mendes')
-          slack_message = FactoryBot.create(:slack_message, ts: '123')
-          FactoryBot.create(:pull_request, source_control_id: 180, slack_message:, user:,
-                                           repository:, head: 'kaiomagalhaes-patch-121')
-          valid_json_direct_message = valid_json.deep_dup
-          valid_json_direct_message[:review][:state] = 'test'
-          valid_json_direct_message[:review][:body] = ''
-
-          flow = described_class.new(valid_json_direct_message)
-
-          expect_any_instance_of(Clients::Notifications::Direct).to receive(:send).with(
-            ':boom: there are conflicts on this Pull Request: <https://github.com/codelittinc/gh-hooks-repo-test/pull/180|gh-hooks-repo-test#180>', 'rheniery.mendes'
-          )
-
-          flow.run
-        end
-      end
-
       it 'does not send a message if the user does not have a slack username' do
         VCR.use_cassette('flows#new-review-submission-request#new-review-send-direct-message') do
           user = FactoryBot.create(:user, slack: nil)
@@ -253,6 +233,7 @@ RSpec.describe Flows::Repositories::PullRequest::CodeReview::Flow, type: :servic
         end
       end
     end
+
     describe '#run' do
       it 'sends a message if there is a new review submission' do
         VCR.use_cassette('flows#new-review-submission-request#azure#new-review-send-message') do
@@ -270,6 +251,28 @@ RSpec.describe Flows::Repositories::PullRequest::CodeReview::Flow, type: :servic
         end
       end
 
+      it ' does not create or send a message if one equal was created in less than 5 minutes' do
+        VCR.use_cassette('flows#new-review-submission-request#azure#new-review-send-message') do
+          slack_message = FactoryBot.create(:slack_message, ts: '123')
+          pr = FactoryBot.create(:pull_request, source_control_id: 357, repository:,
+                                           slack_message:, head: 'kaiomagalhaes-patch-121', source_control_type: 'azure')
+
+          flow = described_class.new(valid_json)
+
+            PullRequestReview.create!(
+              pull_request: pr,
+              username: 'kaio@codelitt.com',
+              state: 'commented',
+              backstage_user_id: 123
+            )
+
+          expect_any_instance_of(Clients::Notifications::Channel).not_to receive(:send).with(
+            ':speech_balloon: There is a new message!', 'feed-test-automations', '123'
+          )
+
+          flow.run
+        end
+      end
       # @TODO: fix these tests
       # it 'sends the correct channel message when there are changes requested' do
       #  VCR.use_cassette('flows#new-review-submission-request#new-review-send-channel-message') do
