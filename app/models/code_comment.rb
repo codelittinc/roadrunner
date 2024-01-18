bbdfecbae3077a6e2c00b8e1d7cc44a1f9ff07f9
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-01-18 14:41:46 -0300

    create code comments model

---
+++ app/models/code_comment.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: code_comments
+#
+#  id              :bigint           not null, primary key
+#  comment         :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  author_id       :integer
+#  pull_request_id :bigint           not null
+#
+# Indexes
+#
+#  index_code_comments_on_pull_request_id  (pull_request_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (pull_request_id => pull_requests.id)
+#
+class CodeComment < ApplicationRecord
+  belongs_to :pull_request
+
+  validates :comment, presence: true
+  validates :author_id, presence: true
+end
---
+++ db/migrate/20240118173723_create_code_comments.rb
@@ -0,0 +1,11 @@
+class CreateCodeComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :code_comments do |t|
+      t.integer :author_id
+      t.references :pull_request, null: false, foreign_key: true
+      t.string :comment
+
+      t.timestamps
+    end
+  end
+end
