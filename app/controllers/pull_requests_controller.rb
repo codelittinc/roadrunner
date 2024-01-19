69d2bfcdb7d50081960f5c8dd2e2d33b621d7609
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-01-19 19:03:55 -0300

    add link to pull requests

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -11,11 +11,9 @@ class PullRequestsController < ApplicationController
     @pull_requests = PullRequest.where(created_at: start_date..end_date, state:)
 
     project_id = params[:project_id]
-    return unless project_id
+    @pull_requests = @pull_requests.joins(:repository).where(repository: { external_project_id: project_id }) if project_id
 
-    @pull_requests = @pull_requests.joins(:repository).where(repository: { external_project_id: project_id })
-
-    backstage_user_id = params[:backstage_user_id]
+    backstage_user_id = params[:user_id]
     return unless backstage_user_id
 
     @pull_requests = @pull_requests.where(backstage_user_id:)
