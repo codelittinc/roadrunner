28b994a40cfbbcadb3acb79b24d0a6109f911ef4
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-01-19 14:31:46 -0300

    add published at to code comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_18_173723) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_19_171222) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_18_173723) do
     t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "published_at"
     t.index ["pull_request_id"], name: "index_code_comments_on_pull_request_id"
   end
 
