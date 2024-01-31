d3ba5ab09d7add30b3c1a90a57bf784a1b63d32e
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-01-31 14:37:31 -0300

    fix commits matcher

--- app/services/commits_matcher.rb
+++ app/services/commits_matcher.rb
@@ -3,21 +3,19 @@
 class CommitsMatcher
   # @TODO Receive project
   # @TODO Do not use message as a unique key to get commits
-  def initialize(github_commits)
-    @github_commits = github_commits
+  def initialize(source_control_commits)
+    @source_control_commits = source_control_commits
   end
 
   def commits
-    commits = []
-    @github_commits.each do |commit|
-      message = commit.message
-      c = Commit.order(created_at: :desc)
-                .where.not(id: commits&.map(&:id))
-                .where(message:).first
+    commits_list = []
+    @source_control_commits.each do |commit|
+      message = commit.message[0..50]
+      change = Commit.order(created_at: :desc).where.not(id: commits_list&.map(&:id)).where('message LIKE ?', "%#{message}%").first
 
-      commits << c if c
+      commits_list << change if change
     end
 
-    commits.sort_by(&:created_at)
+    commits_list.sort_by(&:created_at)
   end
 end
