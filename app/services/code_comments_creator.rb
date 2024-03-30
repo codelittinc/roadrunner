bc41bc86836bea423f7117289cccd9d34ef9b18e
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-03-30 16:41:40 -0300

    remove error verification from the update comments

--- app/services/code_comments_creator.rb
+++ app/services/code_comments_creator.rb
@@ -10,7 +10,7 @@ class CodeCommentsCreator
     comments = @source_control_pull_request_client&.new&.comments(@pull_request.repository, @pull_request.source_control_id)
 
     comments&.each do |comment|
-      raise "Backstage user cannot be nil. Author name is #{comment.author_identifier} and pull request id is #{@pull_request.id}" if comment.author.nil?
+      skip unless comment.author.present?
 
       CodeComment.find_or_create_by!(
         pull_request: @pull_request,
