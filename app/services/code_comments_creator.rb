9e22dff77437f33532e3f07bb08a7e5652d66915
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-03-30 16:16:04 -0300

    fix error

--- app/services/code_comments_creator.rb
+++ app/services/code_comments_creator.rb
@@ -8,9 +8,10 @@ class CodeCommentsCreator
 
   def create
     comments = @source_control_pull_request_client&.new&.comments(@pull_request.repository, @pull_request.source_control_id)
-    raise "Backstage user cannot be nil. Author name is #{comment.author} and pull request id is #{@pull_request.id}" if comment.author.nil?
 
     comments&.each do |comment|
+      raise "Backstage user cannot be nil. Author name is #{comment.author} and pull request id is #{@pull_request.id}" if comment.author.nil?
+
       CodeComment.find_or_create_by!(
         pull_request: @pull_request,
         comment: comment.comment,
