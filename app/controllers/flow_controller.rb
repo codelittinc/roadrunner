350f41ce985a9e58f9548355df588a8c44bda6b2
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-05-30 14:31:13 -0300

    add zoom url verification in the flow controller

--- app/controllers/flow_controller.rb
+++ app/controllers/flow_controller.rb
@@ -6,6 +6,7 @@ class FlowController < ApplicationController
     # value to validate the ownership of the subscriber.
     # More info: https://api.slack.com/apis/connections/events-api#handshake
     return render json: { challenge: params['challenge'] }, status: :ok if params['challenge']
+    return zoom_challenge if params.dig('payload', 'plainToken')
 
     json = (params[:flow] || params).merge(request.GET).to_json
     flow_request = FlowRequest.create!(json:)
@@ -17,5 +18,20 @@ class FlowController < ApplicationController
     end
 
     render json: { text: 'Roadrunner is processing your request.' }, status: :ok
+  end
+
+  private
+
+  def zoom_challenge
+    plain_token = params.dig('payload', 'plainToken')
+    secret = ENV.fetch('ZOOM_WEBHOOK_SECRET_TOKEN', nil)
+    encrypted_token = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), secret, plain_token)
+
+    response_json = {
+      plainToken: plain_token,
+      encryptedToken: encrypted_token
+    }
+
+    render json: response_json, status: :ok
   end
 end
