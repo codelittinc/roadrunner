6136046d2040cce84e312c6cba58329a93cdad81
Kaio Magalhaes <me@kaiomagalhaes.com>
2025-07-09 09:07:39 -0300

    fix backstage user

--- app/services/clients/backstage/user.rb
+++ app/services/clients/backstage/user.rb
@@ -20,8 +20,16 @@ module Clients
     class User < Client
       def list(*query)
         query_string = query.join(',')
-        response = Request.get("#{@url}/users?query=#{query_string}", authorization)
+        response = get_users(query_string)
         response.map { |user| BackstageUser.new(user) }
+      end
+
+      def get_users(query_string)
+        uri = URI.parse(build_url("/users?query=#{query_string}"))
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        req = Net::HTTP::Get.new(uri.request_uri, { 'Project-Auth-Key' => authorization })
+        JSON.parse(http.request(req).body)
       end
     end
   end
